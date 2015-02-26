b017e522c7dfce2b337c426c3e014d14407fdb1a
a110482 <a110482@yahoo.com.tw>
2015-02-26 19:58:55 +0800

    first_1.0.1 nav注解完成，bug標註於最下方

--- static/proto_demo/js/project_status.js
+++ static/proto_demo/js/project_status.js
@@ -4,17 +4,27 @@ var width_move = function() {
     $('.status_backers_sec').addClass('status_backers_sec_phone');
 }//if判斷式的公用部分
 var main = function() {
+    var flag=0,timer,timer_2;
     $('.logo').hover(function(){
         $('.logo').toggleClass('change');
     });//切換LOGE顏色
-    $('.nav').mouseenter(function(){clearTimeout(timer_2);});//滑鼠移入ＮＡＶ取消縮回倒數
-    $('.nav').mouseleave(function(){timer_2=setTimeout(function(){$('.nav').slideUp(500);},1000);});//滑鼠移出時倒數縮回
+    $('.nav').mouseenter(function(){
+        clearTimeout(timer_2);
+        flag=1;
+    });//滑鼠移入ＮＡＶ取消縮回倒數
+    $('.nav').mouseleave(function() {
+        if ($(document).scrollTop() > 0) {//避免在頂端時縮回
+        timer_2 = setTimeout(function () {
+            $('.nav').slideUp(500);
+            setTimeout(function(){flag=0},500);
+        }, 1000);
+        }
+    });//滑鼠移出時倒數縮回
     var height_1 = $(window).height();//取得視窗取得視窗高度
     var width_2 = $(window).width();   //視窗實際寬度
     if (width_2 < 600){
         $(width_move());
     }
-var flag=0,timer,timer_2;
     var scrollTest=function(){
         if($(document).scrollTop()==0){
             $('.nav').addClass('test').slideDown(500);
@@ -23,12 +33,12 @@ var flag=0,timer,timer_2;
             var height_top = $(document).scrollTop();
             timer = setTimeout(function () {
                 var height_top_2 = $(document).scrollTop();
-                if (height_top_2 - height_top > 0 && flag==0) {//利用１０毫秒差距的高度偵測往上還是往下
+                if (height_top_2 - height_top > 0) {//利用１０毫秒差距的高度偵測往上還是往下
                     flag=1;
                     $('.nav').slideUp(500);
                     setTimeout(function(){flag=0},500);//滑動作用期間不偵測滑棒動作
                 }
-                else if (height_top_2 - height_top < 0 && flag==0) {
+                else if (height_top_2 - height_top < 0) {
                     flag=1;
                     $('.nav').addClass('test').slideDown(500);
                     clearTimeout(timer_2);
@@ -73,3 +83,5 @@ $(window).resize(function () {
         $('.status_backers_sec').removeClass('status_backers_phone');
     }
 });//滑鼠縮放事件
+
+//BUGS  1.滑鼠一開始太上面時，無法鎖定nav　　２.滑鼠鎖定nav，離開時繼續往上滾，會先縮回一次再出現。
