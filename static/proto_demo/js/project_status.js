2cc421ccab498b4472238987bf39d4a520159d90
a110482 <a110482@yahoo.com.tw>
2015-03-26 16:27:38 +0800

    1.2.2   CODE改結構

--- static/proto_demo/js/project_status.js
+++ static/proto_demo/js/project_status.js
@@ -14,16 +14,25 @@ $(window).resize(function () {
         $('.status_backers_sec').removeClass('status_backers_phone');
     }
 });//滑鼠縮放事件
+var functionDatabase = function(){
+    function MonDay(year,month,day){
+        var toDay_1 = new Date(year,month,day);//將日期存入變數
+        for (i=0;toDay_1.getDay() != 1;i++){//運算到找到禮拜一為止
+            var i_2=i*24;//一天24小時
+            toDay_1.setHours(day-i_2);//迴圈每跑一次就減去一天
+        }
+        return toDay_1
+    }//輸入日期會找出當周星期一的日期，手輸月分會加一個月，系統用陣列記錄月份
+}
 
-var main = function() {
+$(document).ready( function() {
     var height_1 = $(window).height();//取得視窗取得視窗高度
     var width_2 = $(window).width();   //視窗實際寬度
     if (width_2 < 600){
         $(widthChange());
     }//　切換手機版本
-}//主文件
-
-var navSlideEvent = function(){
+});
+$(document).ready(function(){
     var flag=0,timer,timer_2;//nav使用變數
     $('.logo').hover(function(){
         $('.logo').toggleClass('change');
@@ -50,38 +59,37 @@ var navSlideEvent = function(){
         if(flag==0 || flag==1) {//避免動作重複偵測
             height_top_2 = $(document).scrollTop();
             if (height_top_2 - height_top > 0) {//往下，利用１０毫秒差距的高度偵測往上還是往下
-                    flag=2;
-                    $('.nav').slideUp(200);//往上縮回
-                    setTimeout(function(){
-                        flag=0;
-                        height_top = $(document).scrollTop();
-                    },200);//滑動作用期間不偵測滑棒動作
-                }
-                else if (height_top_2 - height_top < 0 && flag==0) {
-                    flag=1;
-                    $('.nav').addClass('navSlide').slideDown(200);
-                    setTimeout(function(){flag=0
-                        height_top = $(document).scrollTop();
-                    },200);
-                    clearTimeout(timer_2);//如過滑鼠繼續往上，停止縮回計時
-                    timer_2=setTimeout(function(){$('.nav').slideUp(200);
-                    },3000);//閒置時ＮＡＶ３秒縮回
-                }//滑鼠往上
-            };
-        }
+                flag=2;
+                $('.nav').slideUp(200);//往上縮回
+                setTimeout(function(){
+                    flag=0;
+                    height_top = $(document).scrollTop();
+                },200);//滑動作用期間不偵測滑棒動作
+            }
+            else if (height_top_2 - height_top < 0 && flag==0) {
+                flag=1;
+                $('.nav').addClass('navSlide').slideDown(200);
+                setTimeout(function(){flag=0
+                    height_top = $(document).scrollTop();
+                },200);
+                clearTimeout(timer_2);//如過滑鼠繼續往上，停止縮回計時
+                timer_2=setTimeout(function(){$('.nav').slideUp(200);
+                },3000);//閒置時ＮＡＶ３秒縮回
+            }//滑鼠往上
+        };
+    }
     window.addEventListener("scroll",navSlide,false);//監聽滑棒
-}//nac滑動功能
-
-var test=function(){
+});
+$(document).ready(function(){
     $('.btn_2').click(function(){
         $('.input').show();
     });
     $('.closeBtn').click(function(){
         $('.input').hide();
     });
-}//測試區
+});
 
-var timeForm = function(){
+$(document).ready(function(){
     function MonDay(){
         var toDay_1 = new Date();//將日期存入變數
         for (i=0;toDay_1.getDay() != 1;i++){//運算到找到禮拜一為止
@@ -103,25 +111,6 @@ var timeForm = function(){
     $("#thd").prepend('<div id="'+thdDay.getMonth()+"M"+thdDay.getDate()+"D"+'">'+thdDay.getMonth()+"M"+thdDay.getDate()+"D"+'</div>');
     var thrDay =new Date(MonDay().setHours(MonDay().getHours()+48));
     $("#thr").prepend('<div id="'+thrDay.getMonth()+"M"+thrDay.getDate()+"D"+'">'+thrDay.getMonth()+"M"+thrDay.getDate()+"D"+'</div>');
-}
-
-var functionDatabase = function(){
-    function MonDay(year,month,day){
-        var toDay_1 = new Date(year,month,day);//將日期存入變數
-        for (i=0;toDay_1.getDay() != 1;i++){//運算到找到禮拜一為止
-            var i_2=i*24;//一天24小時
-            toDay_1.setHours(day-i_2);//迴圈每跑一次就減去一天
-        }
-        return toDay_1
-    }//輸入日期會找出當周星期一的日期，手輸月分會加一個月，系統用陣列記錄月份
-}
-
-$(document).ready(main);
-$(document).ready(navSlideEvent);
-$(document).ready(test);
-$(document).ready(timeForm);
+});
 
 
-//BUGS  1.滑鼠一開始太上面時，無法鎖定nav　
-
-//啟動先延時10ms定位，滾動後再定位一次計算差值
