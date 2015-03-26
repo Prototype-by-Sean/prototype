84ba5b9d29ad84ce4ada8a11c0a07f1aedb2c9c3
a110482 <a110482@yahoo.com.tw>
2015-03-26 15:11:59 +0800

    1.2.1   新增  日期排序測試版

--- static/proto_demo/js/project_status.js
+++ static/proto_demo/js/project_status.js
@@ -36,7 +36,7 @@ var navSlideEvent = function(){
         if ($(document).scrollTop() > 0) {//避免在頂端時縮回
             flag=0;
             timer_2 = setTimeout(function () {
-                $('.nav').slideUp(500);
+                $('.nav').slideUp(200);
             }, 1000);
         }
     });//滑鼠移出時倒數縮回
@@ -51,20 +51,20 @@ var navSlideEvent = function(){
             height_top_2 = $(document).scrollTop();
             if (height_top_2 - height_top > 0) {//往下，利用１０毫秒差距的高度偵測往上還是往下
                     flag=2;
-                    $('.nav').slideUp(500);//往上縮回
+                    $('.nav').slideUp(200);//往上縮回
                     setTimeout(function(){
                         flag=0;
                         height_top = $(document).scrollTop();
-                    },500);//滑動作用期間不偵測滑棒動作
+                    },200);//滑動作用期間不偵測滑棒動作
                 }
                 else if (height_top_2 - height_top < 0 && flag==0) {
                     flag=1;
-                    $('.nav').addClass('navSlide').slideDown(500);
+                    $('.nav').addClass('navSlide').slideDown(200);
                     setTimeout(function(){flag=0
                         height_top = $(document).scrollTop();
-                    },500);
+                    },200);
                     clearTimeout(timer_2);//如過滑鼠繼續往上，停止縮回計時
-                    timer_2=setTimeout(function(){$('.nav').slideUp(500);
+                    timer_2=setTimeout(function(){$('.nav').slideUp(200);
                     },3000);//閒置時ＮＡＶ３秒縮回
                 }//滑鼠往上
             };
@@ -73,26 +73,39 @@ var navSlideEvent = function(){
 }//nac滑動功能
 
 var test=function(){
-    $('.testBtn').click(function(){
+    $('.btn_2').click(function(){
         $('.input').show();
     });
     $('.closeBtn').click(function(){
         $('.input').hide();
     });
-    var DateIn=new Date();
-    var testText=document.getElementById("test");
-    testText.textContent=ToDay(DateIn.getFullYear(),DateIn.getMonth(),DateIn.getDate());
-    function ToDay(year,month,day){
-        var toDay_1 = new Date(year,month,day);//將日期存入變數
-        for (i=0;toDay_1.getDay() != 1;i++){//運算到找到禮拜一為止
-            var i_2=i*24;//一天24小時
-            toDay_1.setHours(day-i_2);//迴圈每跑一次就減去一天
-        }
-        return toDay_1
-    }//輸入日期會找出當周星期一的日期，手輸月分會加一個月，系統用陣列記錄月份
 }//測試區
 
 var timeForm = function(){
+    function MonDay(){
+        var toDay_1 = new Date();//將日期存入變數
+        for (i=0;toDay_1.getDay() != 1;i++){//運算到找到禮拜一為止
+            var i_2=24;//一天24小時
+            toDay_1.setHours(toDay_1.getHours()-24);//迴圈每跑一次就減去一天
+        }
+        return toDay_1
+    }//輸入日期會找出當周星期一的日期，手輸月分會加一個月，系統用陣列記錄月份
+    function yesterday(today,no){
+        var today_2 = today.setHours(today.getHours()-no*24);
+        alert(today_2);
+        return today_2
+    }
+    //var toDay = MonDay(DateIn.getFullYear(),DateIn.getMonth(),DateIn.getDate());
+    //var TimeId =toDay.getDate();
+
+    $("#fir").prepend('<div id="'+MonDay().getMonth()+"M"+MonDay().getDate()+"D"+'">'+MonDay().getMonth()+"M"+MonDay().getDate()+"D"+'</div>');
+    var thdDay =new Date(MonDay().setHours(MonDay().getHours()+24));
+    $("#thd").prepend('<div id="'+thdDay.getMonth()+"M"+thdDay.getDate()+"D"+'">'+thdDay.getMonth()+"M"+thdDay.getDate()+"D"+'</div>');
+    var thrDay =new Date(MonDay().setHours(MonDay().getHours()+48));
+    $("#thr").prepend('<div id="'+thrDay.getMonth()+"M"+thrDay.getDate()+"D"+'">'+thrDay.getMonth()+"M"+thrDay.getDate()+"D"+'</div>');
+}
+
+var functionDatabase = function(){
     function MonDay(year,month,day){
         var toDay_1 = new Date(year,month,day);//將日期存入變數
         for (i=0;toDay_1.getDay() != 1;i++){//運算到找到禮拜一為止
@@ -101,20 +114,12 @@ var timeForm = function(){
         }
         return toDay_1
     }//輸入日期會找出當周星期一的日期，手輸月分會加一個月，系統用陣列記錄月份
-    var DateIn = new Date();
-    var toDay = MonDay(DateIn.getFullYear(),DateIn.getMonth(),DateIn.getDate());
-    var TimeId =toDay.getDate();
-    $("#fir").prepend('<div id="'+toDay.getMonth()+"M"+toDay.getDate()+"D"+'">'+toDay.getMonth()+"M"+toDay.getDate()+"D"+'</div>');
-    var TimeId_2=toDay.setHours(toDay.getData()-24);
-    $("#thd").prepend('<div id="'+TimeId_2+'">'+TimeId_2+'</div>');
-    var TimeId_3=TimeId-2;;
-    $("#thr").prepend('<div id="'+TimeId_3+'">'+TimeId_3+'</div>');
 }
 
 $(document).ready(main);
 $(document).ready(navSlideEvent);
-//$(document).ready(test);
-//$(document).ready(timeForm);
+$(document).ready(test);
+$(document).ready(timeForm);
 
 
 //BUGS  1.滑鼠一開始太上面時，無法鎖定nav　
