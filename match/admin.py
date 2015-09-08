94b554620dd4f4774b1b82ae5d7fb211dddb19bc
DK <prototype.sean@gmail.com>
2015-09-08 14:07:26 +0800

    9/8 同步

--- match/admin.py
+++ match/admin.py
@@ -5,39 +5,48 @@ from models import *
 
 @admin.register(Member)
 class MemberAdmin(admin.ModelAdmin):
-    fields = ()
+    fields = ('user', 'profile_pic',  'biological_sex',
+              'sexuality', 'height', 'weight', 'birthday',
+              'age', 'blood_type', 'location', 'religion',
+              'relationship', 'relationship_status', )
 
 
 @admin.register(BiologicalSex)
 class BiologicalSexAdmin(admin.ModelAdmin):
-    list_display = ()
+    list_display = ('biological_sex', )
+    fields = ()
 
 
 @admin.register(Sexuality)
 class SexualityAdmin(admin.ModelAdmin):
-    list_display = ()
-
+    list_display = ('sexuality', )
+    fields = ()
 
 @admin.register(RelationShip)
 class RelationShipAdmin(admin.ModelAdmin):
-    list_display = ()
+    list_display = ('relationship', )
+    fields = ()
 
 
 @admin.register(RelationShipStatus)
 class RelationShipStatusAdmin(admin.ModelAdmin):
-    list_display = ()
+    list_display = ('relationship_status', )
+    fields = ()
 
 
 @admin.register(BloodType)
 class BloodTypeAdmin(admin.ModelAdmin):
     list_display = ('blood_type', )
+    fields = ()
 
 
 @admin.register(Location)
 class LocationAdmin(admin.ModelAdmin):
-    list_display = ()
+    list_display = ('location', )
+    fields = ()
 
 
 @admin.register(Religion)
 class ReligionAdmin(admin.ModelAdmin):
-    list_display = ()
+    list_display = ('religion', )
+    fields = ()
