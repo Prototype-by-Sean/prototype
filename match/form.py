a666199de21a69e0f03ea0f2074e236344d9756c
Elijah <a110482@yahoo.com.tw>
2015-09-08 16:08:43 +0800

    1608

--- match/form.py
+++ match/form.py
@@ -15,5 +15,4 @@ class SearchForm(forms.ModelForm):
 
     def __init__(self, submit_title='Submit', *args,  **kwargs):
         super(SearchForm, self).__init__(*args, **kwargs)
-        self.helper.add_input(Submit('submit', submit_title))
 # =========ET=========
