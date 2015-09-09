b64f9910fbe1031f1687f31300678ef0121b9e8b
Elijah <a110482@yahoo.com.tw>
2015-09-09 13:38:39 +0800

    更新NAV

--- match/views.py
+++ match/views.py
@@ -21,12 +21,14 @@ class HomePageView(ListView):
 def search_view(request):
         if request.method =='POST':
                 form_in = request.POST
+                use_info = request.user
                 #for c in form_in:
                         #if c !='submit' and c !='csrfmiddlewaretoken' and len(form_in[c]) != 0:
                                 #分單次搜索根多次搜索 分開查詢
                                 #pass
                 end = str(form_in)
-                return render(request,'match/end.html',{'end':end})
+                end1 = str(use_info)
+                return render(request,'match/end.html',{'end':end,'end1':end1})
         else:
                 form = SearchForm()
                 return render(request,'match/test.html',{'test':form})
