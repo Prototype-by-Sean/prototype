658dafd41c0732f7e659a9ffc85edc6e035208d1
Elijah <a110482@yahoo.com.tw>
2015-09-10 21:39:20 +0800

    更新NAV

--- match/views.py
+++ match/views.py
@@ -119,7 +119,7 @@ def search_view(request):
 
 
                 # dict_in = Member.objects.filter(age__in = form_in['age'])
-                end = form_in
+                end = Member.objects.all().values()
                 return render(request,'match/end.html',{'end':end})
         else:
                 form = SearchForm()
