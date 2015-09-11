3d0f05e5d348052e14ff186a4c9125ee4a93b781
Elijah <a110482@yahoo.com.tw>
2015-09-11 11:54:04 +0800

    過濾器v0.19 修正變數名稱，以資辨認

--- match/filters_view.py
+++ match/filters_view.py
@@ -26,3 +26,10 @@ def find_q_set(model, *args):
         # ========寫入結果=========
     return qs_set
 # ========從資料庫撈取符合對象=========可同時輸入多筆代號   結果會疊加  ex: find_q_set(1,4) 結果為 [異性男] 跟 [異性女] 集合
+
+def filter_age(model,qs_set,*args):
+    # qs_set  為一個優先權字典 {'name':2, 'name2':1.....
+    # args  為年齡上下限
+    args = sorted(args)
+    for c in qs_set:
+        if
--- match/views.py
+++ match/views.py
@@ -20,20 +20,18 @@ class HomePageView(ListView):
         template_name = 'match/home.html'
 
 # =======ET=======
+
+
 def search_view(request):
-        if request.method =='POST':
+        if request.method == 'POST':
                 form_in = request.POST
                 # 字典型態{'a':[1,2],'b':[5,6],'c':[]}
-
-
-                #======定義使用者本身性別======
+                # ======定義使用者本身性別======
                 try:
-                        my_biological_sex = Member.objects.filter(user = str(request.user))
+                        my_biological_sex = Member.objects.filter(user=str(request.user))
                 except:
                         my_biological_sex = None
-                #======定義使用者本身性別======
-
-
+                # ======定義使用者本身性別======
                 # =========分析找男女==========
                 # 輸出 test_biological_sex        1:男女都選或都不選  2:女  3:男
                 test_girl = 0
@@ -62,46 +60,40 @@ def search_view(request):
                                 test_biological_sex = 1
                 del test_girl, test_boy
                 # =========分析找男女==========
-
-
                 # ======生成搜索用queryset======
-                # 已轉換為清單內含字典格式(純PY)    ex : [{'id': 1, 'level_id': 2, 'job_id': 2, 'name': 'ET'}, {'id': 2, 'level_id': 2, 'job.....
+                # 已轉換為清單內含字典格式(純PY)    ex : [{'id': 1, 'level_id': 2, 'job_id': 2, 'n.....
                 qs_set = None
                 if my_biological_sex == '男':
-                        if test_biological_sex == 1 :
+                        if test_biological_sex == 1:
                                 # 男女都撈(只撈性向
-                                qs_set = filters_view.find_q_set(Member,2,3,4,6)
-                        if test_biological_sex == 2 :
+                                qs_set = filters_view.find_q_set(Member, 2, 3, 4, 6)
+                        if test_biological_sex == 2:
                                 # 撈女生
-                                qs_set = filters_view.find_q_set(Member,4,6)
-                        if test_biological_sex == 3 :
+                                qs_set = filters_view.find_q_set(Member, 4, 6)
+                        if test_biological_sex == 3:
                                 # 撈男生
-                                qs_set = filters_view.find_q_set(Member,2,3)
+                                qs_set = filters_view.find_q_set(Member, 2, 3)
                 elif my_biological_sex == '女':
-                        if test_biological_sex == 1 :
+                        if test_biological_sex == 1:
                                 # 男女都撈(只撈性向
-                                qs_set = filters_view.find_q_set(Member,1,3,5,6)
-                        if test_biological_sex == 2 :
+                                qs_set = filters_view.find_q_set(Member, 1, 3, 5, 6)
+                        if test_biological_sex == 2:
                                 # 撈女生
-                                qs_set = filters_view.find_q_set(Member,5,6)
-                        if test_biological_sex == 3 :
+                                qs_set = filters_view.find_q_set(Member, 5, 6)
+                        if test_biological_sex == 3:
                                 # 撈男生
-                                qs_set = filters_view.find_q_set(Member,1,3)
+                                qs_set = filters_view.find_q_set(Member, 1, 3)
                 else:
-                        if test_biological_sex == 1 :
+                        if test_biological_sex == 1:
                                 # 男女都撈(只撈性向
-                                qs_set = filters_view.find_q_set(Member,1,4,2,5,3,6)
-                        if test_biological_sex == 2 :
+                                qs_set = filters_view.find_q_set(Member, 1, 4, 2, 5, 3, 6)
+                        if test_biological_sex == 2:
                                 # 撈女生
-                                qs_set = filters_view.find_q_set(Member,4,6)
-                        if test_biological_sex == 3 :
+                                qs_set = filters_view.find_q_set(Member, 4, 6)
+                        if test_biological_sex == 3:
                                 # 撈男生
-                                qs_set = filters_view.find_q_set(Member,1,3)
+                                qs_set = filters_view.find_q_set(Member, 1, 3)
                 # ======生成搜索用queryset======
-
-
-
-
                 # =========生成年齡範圍==========
                 # 生成 age_range = [10,11,12,13,14,15] 若無設定則生成 0 到 100
                 if len(form_in['age_max']) > 0 and len(form_in['age_min']) > 0:
@@ -109,20 +101,15 @@ def search_view(request):
                 elif len(form_in['age_max']) > 0:
                         age_range = list(range(0,int(form_in['age_max'])+1))
                 elif len(form_in['age_min']) > 0:
-                        age_range = list(range(form_in['age_min'],100))
+                        age_range = list(range(form_in['age_min'], 100))
                 else:
                         age_range = list(range(0,100))
                 # =========生成年齡範圍==========
 
-
-
-
-
-
                 # dict_in = Member.objects.filter(age__in = form_in['age'])
                 end = str(qs_set)
-                return render(request,'match/end.html',{'end':end})
+                return render(request,'match/end.html',{'end': end})
         else:
                 form = SearchForm()
-                return render(request,'match/test.html',{'test':form})
+                return render(request,'match/test.html',{'test': form})
 # =======ET=======
