80835edbccb9079f25b41b72a5fd036b008632c2
Elijah <a110482@yahoo.com.tw>
2015-09-11 21:57:19 +0800

    過濾器v0.19 修正變數名稱，以資辨認

--- match/views.py
+++ match/views.py
@@ -131,7 +131,7 @@ def search_view(request):
             q_set = filters_view.filter_weight(Member,q_set,weight_min,weight_max)
         # ====生成體重範圍丟入filter=====
         # dict_in = Member.objects.filter(age__in = form_in['age'])
-        end = str(form_in)
+        end = request._get_post.__dict__
         end1 = len(form_in['blood_type'])
         return render(request,'match/end.html',{'end': end,'end1':end1})
     else:
