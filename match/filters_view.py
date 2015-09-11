a7d296d09651d3d0580d570985dc83d71ff5ccb8
Elijah <a110482@yahoo.com.tw>
2015-09-11 10:48:05 +0800

    過濾器v0.19 修正變數名稱，以資辨認

--- match/filters_view.py
+++ match/filters_view.py
@@ -2,7 +2,7 @@
 
 
 def find_q_set(model, *args):
-    qs_set = {}
+    qs_set = []
     qs_set_temp = None
     for c in args:
         # ========篩選需求=========
@@ -22,7 +22,7 @@ def find_q_set(model, *args):
 
         # ========寫入結果=========
         for c1 in range(len(qs_set_temp)):
-            qs_set[str(qs_set_temp[c1])] = 0
+            qs_set.append(str(qs_set_temp[c]))
         # ========寫入結果=========
     return qs_set
 # ========從資料庫撈取符合對象=========可同時輸入多筆代號   結果會疊加  ex: find_q_set(1,4) 結果為 [異性男] 跟 [異性女] 集合
