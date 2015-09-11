1cbe84a68d73330c4c328046e1f550455f63e465
Elijah <a110482@yahoo.com.tw>
2015-09-11 12:18:14 +0800

    過濾器v0.19 修正變數名稱，以資辨認

--- match/views.py
+++ match/views.py
@@ -107,7 +107,7 @@ def search_view(request):
                 # =========生成年齡範圍==========
 
                 # dict_in = Member.objects.filter(age__in = form_in['age'])
-                end = str(qs_set)
+                end = Member.objects.get(user = 'prototype').age
                 return render(request,'match/end.html',{'end': end})
         else:
                 form = SearchForm()
